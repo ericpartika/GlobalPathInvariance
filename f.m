482af832ed196e90821602aaf66dd77e126030b5
Eric Partika <ericpartika@Erics-Air-2.attlocal.net>
2022-05-11 16:07:56 -0700

    Updated flow map

--- f.m
+++ f.m
@@ -11,4 +11,6 @@ x2dot = u(1) * sin(x(3)); % v*sin(theta)
 x3dot = u(1) * (tan(x(4)))/(L); % v*(tan(delta))/L; L is length of vehicle
 x4dot = u(2); % delta range is +- 20deg
 
+xdot = [x1dot; x2dot; x3dot; x4dot];
+
 end
